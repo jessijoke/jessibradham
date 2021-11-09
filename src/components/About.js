324bc890d2bbe3dd6a3b90198185da70db0e33bb
jessijoke <jessijoke@yahoo.com>
2021-11-08 23:50:51 -0500

    Finished about section

--- src/components/About.js
+++ src/components/About.js
@@ -8,8 +8,14 @@ class About extends Component {
         }
     }
     render(){
-        return <div className="aboutSection">
-            About Jessi
+        return <div className="aboutSection" id="About">
+            <div className="aboutDescription">
+                <button onClick={()=> window.open("https://docs.google.com/document/d/1A4pYlMUMuHmZsaHD-q7hGPt_WNXYMghYFkGesNF4vXY/edit?usp=sharing")}>Resume</button>
+                <div className="description">Jessica Bradham is a Software Developer living in West Michigan. When she's not coding or designing, she spends her time playing guitar, and hanging out with her two giant dogs.<br /> <br /> Email: Jessibradham@gmail.com</div>
+            </div>
+            <div className="aboutImage">
+                <div className="Photo"></div>
+            </div>
         </div>
     }
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -41,11 +41,11 @@ class Navigation extends Component {
                     <div onClick={() => this.flipMenuBar()} id="menuBar" className="navElement"><i className="fas fa-bars"></i></div>
                     <a href="#Home"><div className="navElement">Home</div></a>
                     <a href="#Portfolio"><div className="navElement">Works</div></a>
-                    <a href="#"><div className="navElement">About</div></a>
-                    <a href="https://github.com/jessijoke" target="_blank"><div className="navElement"><i className="fab fa-github"></i> Github</div></a>
-                    <a href="https://www.youtube.com/channel/UC9esGz2ZTAjUEuoNMqIFXBw" target="_blank"><div className="navElement"><i className="fab fa-youtube"></i> Youtube</div></a>
-                    <a href="https://twitter.com/jessicajoyz" target="_blank"><div className="navElement"><i className="fab fa-twitter"></i> Twitter</div></a>
-                    <a href="https://jessijokes.medium.com/" target="_blank"><div className="navElement"><i className="fab fa-medium"></i> Blog</div></a>
+                    <a href="#About"><div className="navElement">About</div></a>
+                    <a href="https://github.com/jessijoke" target="_blank" rel="noreferrer"><div className="navElement"><i className="fab fa-github"></i> Github</div></a>
+                    <a href="https://www.youtube.com/channel/UC9esGz2ZTAjUEuoNMqIFXBw" target="_blank" rel="noreferrer"><div className="navElement"><i className="fab fa-youtube"></i> Youtube</div></a>
+                    <a href="https://twitter.com/jessicajoyz" target="_blank" rel="noreferrer"><div className="navElement"><i className="fab fa-twitter"></i> Twitter</div></a>
+                    <a href="https://jessijokes.medium.com/" target="_blank" rel="noreferrer"><div className="navElement"><i className="fab fa-medium"></i> Blog</div></a>
                 </div>
             </div>
         )
