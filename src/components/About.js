58214f296eb75697fcc11e037dfe821582d62822
jessijoke <jessijoke@yahoo.com>
2023-11-09 04:32:16 -0500

    minimal about update to try to force page update

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,7 @@ class About extends Component {
         return <div className="aboutSection" id="About">
             <div className="aboutDescription">
                 <button onClick={()=> window.open("https://docs.google.com/document/d/1A4pYlMUMuHmZsaHD-q7hGPt_WNXYMghYFkGesNF4vXY/edit?usp=sharing")}>Resume</button>
-                <div className="description">Jessica Bradham is a Software Developer living in West Michigan. When she's not coding or designing, she spends her time playing guitar, and hanging out with her two giant dogs.<br /> <br /> Email: Jessibradham@gmail.com</div>
+                <div className="description">Jessica Bradham is a Software Developer living in West Michigan. When she's not coding or designing, she spends her time playing guitar, writing comedy, and hanging out with her two giant dogs.<br /> <br /> Email: Jessibradham@gmail.com</div>
             </div>
             <div className="aboutImage">
                 <div className="Photo"></div>
